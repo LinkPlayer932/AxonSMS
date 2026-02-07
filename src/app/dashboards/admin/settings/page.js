ae8109d994baf71946048256a732ba4d3523f40f
LinkPlayer <linkplayer932@gmail.com>
2026-02-07 12:52:09 +0500

    ok

--- src/app/dashboards/admin/settings/page.js
+++ src/app/dashboards/admin/settings/page.js
@@ -1,1 +1,9 @@
-Admin setting
+export default function AdminSettingsPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">
+        Admin Settings
+      </h1>
+    </div>
+  );
+}
